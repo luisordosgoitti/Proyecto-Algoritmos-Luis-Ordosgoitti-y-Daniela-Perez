0bb83b036353a614650b8ec3d454a40d43fee1df
luisordosgoitti <luis.ordosgoitti@correo.unimet.edu.ve>
2025-08-06 19:51:55 -0400

    Update galeria.py
    mostrar departamentos listo

--- galeria.py
+++ galeria.py
@@ -2,14 +2,23 @@ from artista import Artista
 from obra import Obra
 from Departamentos import Departamentos
 from db import *
+import os
 
 class Galeria:
-  None
+    def start(self):
+        self.cargar_departamentos()
+        """Aca va el menu"""
+        
+        self.mostrar_departamentos()
+        
+    def mostrar_departamentos(self):
+        for dep in self.departamentos:
+            dep.show()
 
 """Este Método extrae desde el endpoint de Departamentos cada uno de ellos y los guarda en una lista de objetos de tipo Departamento"""
-def cargar_departamentos():
-    departamentos=[]
-    departamentos_from_db=departamentos_db()
-    for departamento in departamentos_from_db["departments"]:
-        departamentos.append(Departamento(departamento["departmentId"], departamento["displayName"]))
+    def cargar_departamentos(self):
+        self.departamentos=[]
+        departamentos_from_db=departamentos_db()
+        for departamento in departamentos_from_db["departments"]:
+            self.departamentos.append(Departamento(departamento["departmentId"], departamento["displayName"]))
 
