222794072cf68533bfd29016354a8117418c6738
luisordosgoitti <luis.ordosgoitti@correo.unimet.edu.ve>
2025-08-06 15:13:51 -0400

    Update galeria.py

--- galeria.py
+++ galeria.py
@@ -5,7 +5,8 @@ from db import *
 
 class Galeria:
   None
-  
+
+"""Este Método extrae desde el endpoint de Departamentos cada uno de ellos y los guarda en una lista de objetos de tipo Departamento"""
 def cargar_departamentos():
     departamentos=[]
     departamentos_from_db=departamentos_db()
