dc6ed427cfeffadf07f4a686e4471ef10facebff
luisordosgoitti <luis.ordosgoitti@correo.unimet.edu.ve>
2025-08-06 15:16:28 -0400

    Update Departamentos.py
    puse el metodo show y el comentario de la clase

--- Departamentos.py
+++ Departamentos.py
@@ -3,5 +3,11 @@ class Departamento:
     def __init__(self, id, nombre):
         self.id=id
         self.nombre=nombre
-        
 
+    def show (self):
+        print(f"ID: {self.id}")
+        print (f"Nombre del departamento: {self.nombre}")
+        print("\n"+"-"*80)
+
+"""Aca se define la clase departamento, que observando el endpoint de departamentos solo se conforma
+por esos atributos, y luego se define un metodo show para posteriormente mostrar la lista de departamentos"""
