b6381a8cfb3389fb5517fe31689e430ee6c84866
luisordosgoitti <luis.ordosgoitti@correo.unimet.edu.ve>
2025-08-07 12:39:56 -0400

    Update db.py

--- db.py
+++ db.py
@@ -4,8 +4,11 @@ def departamentos_db():
     departamentos_from_api=requests.get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
     return departamentos_from_api.json()
 
-
-
-
-
-
+def nacionalidades_db():
+    archivo_nac=open("Nacionalidades.txt", "r")
+    archivo_nac.readline()
+    nacionalidades=[]
+    for pais in archivo_nac:
+        nacionalidad=pais.strip()
+        nacionalidades.append(nacionalidad)
+    return nacionalidades
