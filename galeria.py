1b6c1898de2d21c295055e2b02dd9151680db685
pdaniela-cloud <p.daniela@correo.unimet.edu.ve>
2025-08-08 09:28:31 -0400

    Update galeria.py

--- galeria.py
+++ galeria.py
@@ -3,8 +3,12 @@ from obra import Obra
 from Departamentos import Departamento
 from db import *
 import os
+import requests
 
 class Galeria:
+    def __init__(self):
+        self.departamentos = []
+
     """Este metodo es el principal del sistema"""
     def start(self):
         self.cargar_departamentos()
@@ -19,19 +23,27 @@ class Galeria:
                 os.system('cls')
                 self.mostrar_departamentos()
                 try:
-                    id_departamento = int(input("Por favor, ingresa un número válido de ID para Departamento: "))
+                    id_departamento = int(input("Por favor, ingrese un número válido de ID para Departamento: "))
+                    self.buscar_obras_por_departamento(id_departamento)
                 except:
                     print("Por favor, ingrese un número de la lista.")
+                    
             elif menu == "2":
                 os.system('cls')
-                self.mostrar_nacionalidades()
-                try:
-                    nacionalidad = int(input("Por favor, ingresa el número de la nacionalidad: "))
-                except:
-                    print("Por favor, ingrese un número de la lista.")  
+                nacionalidades = nacionalidades_db()
+                if nacionalidades:
+                    self.mostrar_nacionalidades()
+                    try:
+                        nacionalidad = input("Por favor, ingrese una nacionalidad (ej. American): ")
+                                self.buscar_obras_por_nacionalidad(nacionalidad)
+                        else:
+                            print("Opción inválida. Vuelva a ingresar otro número.")
+                    except:
+                        print("Por favor, ingrese un número de la lista.")
+                
             elif menu == "3":
                 os.system('cls')
-                nombre_artista = input("Por favor, ingresa el nombre de un artista: ")
+                nombre_artista = input("Por favor, ingrese el nombre de un artista: ")
                 self.buscar_artista(nombre_artista)
                 
             elif menu == "4":
@@ -40,9 +52,34 @@ class Galeria:
             else:
                  print("Opción inválida.")
 
+    """Métodos de Búsqueda"""
     def buscar_artista (self,nombre):
         obras_artista=requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q={nombre}")
-        return obras_artista.json()
+        print(obras_artista.json())
+
+    def buscar_obras_por_departamento (self,id_departamento):
+        obras_departamento=requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId={id_departamento}&q=true")
+        print(obras_departamento.json())
+
+    def buscar_obras_por_nacionalidad (self,nacionalidad_seleccionada):
+        try:
+            parametros_de_busqueda = {
+        "q": nacionalidad_seleccionada
+        "artistNationality": "true"
+        }
+        obras_pais = requests.get(
+        "https://collectionapi.metmuseum.org/public/collection/v1/search",
+        params=parametros_de_busqueda
+        )
+        obras_pais.raise_for_status() 
+        resultados = obras_pais.json()
+        
+        if resultados.get("objectIDs"):
+            print(f"Se encontraron {len(resultados['objectIDs'])} obras de artistas de nacionalidad '{nacionalidad_seleccionada}'.")
+            for object_id in resultados["objectIDs"][:20]:
+                 self.mostrar_detalles_de_obra(object_id)
+        else:
+            print(f"No se encontraron obras de la nacionalidad: {nacionalidad_seleccionada}.")
 
     """Este metodo aplica el metodo show a cada uno de los objetos departamento en la lista creada en cargar departamentos"""
     def mostrar_departamentos(self):
